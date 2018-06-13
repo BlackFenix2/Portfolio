4f0a72276b4896094b46ff9184d03ea20132c5a2
Ernest Francis <devin_98@msn.com>
2018-06-12 21:26:53 -0400

    testing node env

--- client/src/Pages/Test/TestLoading/FullExample.tsx
+++ client/src/Pages/Test/TestLoading/FullExample.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import Fullscreen from 'react-fullscreen-crossbrowser';
 
 const FullExample = () => (
-  <div className="App">
-    <button onClick={() => this.setState({ isFullscreenEnabled: true })}>
-      Go Fullscreen
-    </button>
-    <Fullscreen
-      enabled={false}
-      onChange={isFullscreenEnabled => this.setState({ isFullscreenEnabled })}
-    >
-      <div className="full-screenable-node">
-        Hi! This may cover the entire monitor.
-      </div>
-    </Fullscreen>
+  <div>
+    <p>{String(process.env.NODE_ENV_STAGING)}</p>
   </div>
 );
 
