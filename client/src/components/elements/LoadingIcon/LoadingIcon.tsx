7c9a8c1eb7d6c7800ed19e05808a24757e88f3f8
Ernest Francis <devin_98@msn.com>
2018-07-18 11:04:44 -0400

    hotifx for loading icon

--- client/src/components/elements/LoadingIcon/LoadingIcon.tsx
+++ client/src/components/elements/LoadingIcon/LoadingIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Icon } from 'semantic-ui-react';
 
-const LoadingIcon = props => <Icon name="sync" spin={props.active} />;
+const LoadingIcon = props => <Icon name="sync" loading={props.active} />;
 
 export default LoadingIcon;
