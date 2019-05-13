3e071d5954d665d1dfc201251d6af8b6404409a7
blackfenix2 <devin_98@msn.com>
2019-05-13 19:25:33 -0400

    testing React Hooks

--- client/src/Pages/Test/Styled/Styled.tsx
+++ client/src/Pages/Test/Styled/Styled.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 
-import { css, jsx } from '@emotion/core';
+import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 
 import LoadingIcon from 'src/components/elements/LoadingIcon';
 
-const Button = props => (
+const Button: React.FC<any> = props => (
   <StyledButton onClick={props.onClick} className={props.className}>
     {props.children}
   </StyledButton>
 );
 
-const ButtonContainer = props => (
+const ButtonContainer: React.FC<any> = props => (
   <Button {...props}>
     {props.children} <LoadingIcon active={props.loading} />
   </Button>
 );
 
 const StyledButton = styled.button`
+  text-align: center;
   background-color: black;
   color: white;
   border: none;
@@ -44,31 +45,31 @@ interface StyledState {
   activeIndex: number;
 }
 
-class Styled extends React.Component<StyledProps, StyledState> {
-  state = {
+const Styled: React.FC<StyledProps> = () => {
+  const [style, setStyle] = React.useState<StyledState>({
     loading: false,
     activeIndex: 0
+  });
+
+  React.useEffect(() => {
+    console.log('loading test start');
+    return () => console.log('loading end');
+  }, [style.loading]);
+
+  const onChangeEvent = () => {
+    setStyle({ ...style, loading: !style.loading });
   };
-
-  clickEvent = () => {
-    this.setState({
-      loading: !this.state.loading
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <h2>Styled Tests</h2>
-        <button className="w3-button w3-blue">Classic CSS</button>
-        <button css={test}>Styled Core</button>
-        <ButtonContainer onClick={this.clickEvent} loading={this.state.loading}>
-          Styled Button Component
-        </ButtonContainer>
-        <StyledButton onClick={this.clickEvent}>Styled Button</StyledButton>
-      </>
-    );
-  }
-}
-
+  return (
+    <>
+      <h2>Styled Tests</h2>
+      <h4>bleh</h4>
+      <button className="w3-button w3-blue">Classic CSS</button>
+      <button css={test}>Styled Core</button>
+      <ButtonContainer onClick={onChangeEvent} loading={style.loading}>
+        Styled Button Component
+      </ButtonContainer>
+      <StyledButton onClick={onChangeEvent}>Styled Button</StyledButton>
+    </>
+  );
+};
 export default Styled;
