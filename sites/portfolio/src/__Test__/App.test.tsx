93557b482fadbae1867c5066d91ec09fcaf5266e
blackfenix2 <devin_98@msn.com>
2020-03-12 20:49:50 -0400

    hotfix, forgot to remove test page

--- sites/portfolio/src/__Test__/App.test.tsx
+++ sites/portfolio/src/__Test__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from 'src/App/App';
+import App from 'src/layouts/App';
 
 describe('App', () => {
   const wrapper = shallow(<App />);
