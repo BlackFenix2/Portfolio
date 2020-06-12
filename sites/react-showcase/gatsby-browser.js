284a642f590581d993685488d95b6611b787b318
blackfenix2 <devin_98@msn.com>
2020-06-12 12:39:50 -0400

    adding confirm window to reset PWA cache

---
+++ sites/react-showcase/gatsby-browser.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-alert */
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
+
+export default {};
