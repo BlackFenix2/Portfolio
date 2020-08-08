7b323dd048c976ddd1c201ad2fef8f92aff577b1
blackfenix2 <devin_98@msn.com>
2020-08-08 01:30:39 -0400

    added quick SEO

--- sites/portfolio/src/pages/_app.tsx
+++ sites/portfolio/src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import Layout from 'src/layout';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   React.useEffect(() => {
@@ -13,9 +14,24 @@ const App = ({ Component, pageProps }: AppProps) => {
   }, []);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Ernie Francis</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ernie Francis" />
+        <meta
+          property="og:description"
+          content="Portfolio site for Ernie Francis IV"
+        />
+        <meta
+          name="description"
+          content="Portfolio site for Ernie Francis IV"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
