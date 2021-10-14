63e49a0e9c2716cdf01b90775e9fef5229774d2f
blackfenix2 <devin_98@msn.com>
2021-10-13 23:07:37 -0400

    changed EOL countdown from flash to IE11

--- sites/react-showcase/src/pages/index.tsx
+++ sites/react-showcase/src/pages/index.tsx
@@ -3,14 +3,14 @@ import CountDown from 'src/components/modules/Countdown';
 import SEO from 'src/components/modules/SEO';
 import { PageProps } from 'gatsby';
 
-const flashEolDate = new Date(2020, 11, 31);
+const flashEolDate = new Date(2022, 6, 15);
 const Home = (props: PageProps) => {
   return (
     <div>
       <SEO title="Home Page" />
       <CountDown
         date={flashEolDate}
-        label={`Flash End of Life - ${flashEolDate.toDateString()}`}
+        label={`IE11 End of Life - ${flashEolDate.toDateString()}`}
       />
     </div>
   );
